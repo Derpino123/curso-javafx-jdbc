bcabdee05399af0388bfe598cc2f4645efcebadc
David <daavidkarlos@gmail.com>
2019-03-31 17:36:29 -0300

    Departamento Controller

---
+++ src/gui/DepartamentoController.java
@@ -0,0 +1,47 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Departamento;
+
+public class DepartamentoController implements Initializable {
+
+	@FXML
+	private TableView<Departamento> tableViewDepartamento;
+
+	@FXML
+	private TableColumn<Departamento, Integer> tableColumnId;
+
+	@FXML
+	private TableColumn<Departamento, String> tableColumnName;
+
+	@FXML
+	private Button btNovoDepartamento;
+
+	@FXML
+	public void onBtNovoDepartamento() {
+		System.out.println("onBtNewAction");
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+
+	private void initializeNodes() {
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		
+		Stage stage = (Stage) Main.getMainScene().getWindow();
+		tableViewDepartamento.prefHeightProperty().bind(stage.heightProperty());
+	}
+}
